47ff688b399bc621c46f44cca1673e9bc76c81e8
Marcel Otto <marcelotto@gmx.de>
2019-09-09 00:21:42 +0200

    Add formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    # TODO: This should be an export from RDF.ex
+    defvocab: 2
+  ]
 ]
