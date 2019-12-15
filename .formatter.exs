31002953a029b80d215f21734dafb7ee687e94db
Marcel Otto <marcelotto@gmx.de>
2019-12-16 00:14:46 +0100

    Import RDF.ex formatter rules

--- .formatter.exs
+++ .formatter.exs
@@ -1,8 +1,5 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    # TODO: This should be an export from RDF.ex
-    defvocab: 2
-  ]
+  import_deps: [:rdf]
 ]
